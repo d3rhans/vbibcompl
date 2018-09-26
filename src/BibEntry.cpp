9cc7b9ac4bbbb514b78fc60451249c09dac67d35
Hans Hohenfeld <hans.hohenfeld@posteo.de>
2018-09-26 23:41:02 +0200

    Bib Entry

---
+++ src/BibEntry.cpp
@@ -0,0 +1,54 @@
+
+#include "BibEntry.h"
+
+struct vbc::BibEntry::BibEntryD
+{
+    MapType data;
+};
+
+vbc::BibEntry::BibEntry(const ValueType& bibKey, const ValueType& bibType)
+    : d(std::make_unique<BibEntryD>())
+{
+    const KeyType BIBKEY{"bibkey"};
+    const KeyType BIBTYPE{"bibtype"};
+
+    d->data.emplace(BIBKEY, bibKey);
+    d->data.emplace(BIBTYPE, bibType);
+}
+
+vbc::BibEntry::~BibEntry() = default;
+
+void vbc::BibEntry::addToken(const KeyType& key, const ValueType& value)
+{
+    d->data.emplace(key, value);
+}
+
+vbc::BibEntry::iterator vbc::BibEntry::begin()
+{
+    return d->data.begin();
+}
+
+vbc::BibEntry::const_iterator vbc::BibEntry::begin() const
+{
+    return d->data.begin();
+}
+
+vbc::BibEntry::const_iterator vbc::BibEntry::cbegin() const
+{
+    return d->data.cbegin();
+}
+
+vbc::BibEntry::iterator vbc::BibEntry::end()
+{
+    return d->data.end();
+}
+
+vbc::BibEntry::const_iterator vbc::BibEntry::end() const
+{
+    return d->data.end();
+}
+
+vbc::BibEntry::const_iterator vbc::BibEntry::cend() const
+{
+    return d->data.cend();
+}
